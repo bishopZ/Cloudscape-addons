306dff672b8bf373590a2c9213fdaf5e07521c7c
Bishop Zareh <xchicago@gmail.com>
2025-07-14 16:14:31 -0600

    add pdf support

--- webpack.config.js
+++ webpack.config.js
@@ -83,7 +83,7 @@ const config = {
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|pdf)$/i,
         type: 'asset/resource',
       },
     ],
