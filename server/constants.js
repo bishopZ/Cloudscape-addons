a89db9313cbaa2cd29732dbe5841c9f001ea194d
Bishop Zareh <xchicago@gmail.com>
2025-07-14 16:12:19 -0600

    add resume

--- server/constants.js
+++ server/constants.js
@@ -2,5 +2,5 @@
 export const POST_TITLE = ' - BishopZ.com'
 export const DEFAULT_TITLE = 'Bishop Z'
 export const DEFAULT_DESCRIPTION = 'Explore technology with Bishop Z\'s insightful articles and resources. Discover the power of AI and design systems, and learn how to integrate them into your daily work.'
-export const DEFAULT_IMAGE = 'https://bishopz.com/chasm.jpg'
+export const DEFAULT_IMAGE = 'https://bishopz.com/assets/chasm.jpg'
 
